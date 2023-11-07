6a8606f1c7580966adf58c68cc3b374b0132e45c
kalderlunt <kalderlunt@gmail.com>
2023-11-08 00:10:37 +0100

    SetupWindow
    Create files :
    - window.c
    - window.h
    
    modifications :
    - add Release in x64
    - main.c

--- Q3_Asteroid/src/main.c
+++ Q3_Asteroid/src/main.c
@@ -2,35 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#include <SFML/Window.h>
-#include <SFML/Audio.h>
 #include <SFML/Graphics.h>
-#include <SFML/System.h>
 
-typedef int bool;
-#define true 1
-#define false 0
-
-int WINDOW_X = 1200;
-int WINDOW_Y = 1200;
-
-float delta = 0;
-
+/*float delta = 0;
 sfClock* deltaclock;
-sfClock* animclock;
-int nframe = 0;
-int anim_time = 75; 
-
-sfFont* font1;
-sfRenderWindow* window;
-
 int Delta() {
     sfTime dtime = sfClock_getElapsedTime(deltaclock);
     delta = sfTime_asMilliseconds(dtime);
     sfClock_restart(deltaclock);
-}
-
-int create() {
+}*/
+/*int create() {
     WINDOW_X = sfVideoMode_getDesktopMode().width;
     WINDOW_Y = sfVideoMode_getDesktopMode().height;
     sfVideoMode mode = { WINDOW_X, WINDOW_Y, 32 };
@@ -41,49 +22,70 @@ int create() {
     deltaclock = sfClock_create();
     animclock = sfClock_create();
     font1 = sfFont_createFromFile("Font/RetroGaming.ttf");
+}*/
 
-    
+void InitWindow(sfRenderWindow** window) {
+    int windowWidth = sfVideoMode_getDesktopMode().width / 2;
+    int windowHeight = sfVideoMode_getDesktopMode().height / 2;
+    // Window initialization
+    sfVideoMode mode = { windowWidth, windowHeight, 32 };
+    *window = sfRenderWindow_create(mode, "Asteroid", sfClose, NULL);
+
+    if (!*window) {
+        // Failure management
+        exit(EXIT_FAILURE);
+    }
+}
+void InitGame(sfRenderWindow** window) {
+    InitWindow(window);
 }
 
-int main() {
-    srand(time(0));
-    create();
 
 
-    sfMusic* music = sfMusic_createFromFile("asset/Music/Castelvania 2.wav");
-
-    
-
-
-
-
+void Game(sfRenderWindow* window) {
+    // Main game loop
     while (sfRenderWindow_isOpen(window)) {
         sfEvent event;
         while (sfRenderWindow_pollEvent(window, &event)) {
-            if (event.type == sfEvtClosed)
+            if (event.type == sfEvtClosed) {
                 sfRenderWindow_close(window);
+            }
         }
 
-        sfMusic_play(music);
-        
-        Delta();
+        // Game update logic
+        if (sfKeyboard_isKeyPressed(sfKeyEscape)) { sfRenderWindow_close(window); } // quit
+            //UpdateLogic();
 
-        if (sfKeyboard_isKeyPressed(sfKeyEscape)) { sfRenderWindow_close(window); } //quit
-
-        ////// DRAW /////
-        sfTime atime = sfClock_getElapsedTime(animclock); //for animated frames
-        int amseconds = sfTime_asMilliseconds(atime);
-        if (amseconds >= anim_time) {
-            nframe += 1;
-            sfClock_restart(animclock);
-        }
+        // Clear screen
         sfRenderWindow_clear(window, sfTransparent);
 
-        sfMusic_destroy(music);
+        // Draw your elements
+            //UpdateDraw();
+        
+        // Display screen
         sfRenderWindow_display(window);
-        /////////////////
     }
+}
 
-    sfClock_destroy(deltaclock);
+
+void ReleaseWindow(sfRenderWindow* window) {
+    sfRenderWindow_destroy(window);
+}
+void ReleaseResources(sfRenderWindow* window) {
+    // Liberating resources
+    ReleaseWindow(window);
+}
+
+
+int main() {
+
+    sfRenderWindow* window = NULL;
+
+    InitGame(&window);
+
+    Game(window);
+
+    ReleaseResources(window);
+
     return 0;
 }
